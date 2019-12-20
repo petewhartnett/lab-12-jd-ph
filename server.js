a234f86eeab4a927b17a4c4a18637025b5599baf
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-12-19 23:35:22 -0800

    tutor help

--- server.js
+++ server.js
@@ -37,57 +37,46 @@ app.get('/savedBook', (req, res) => {
 app.get('/', (req, res) => {
   const instruction = 'SELECT * FROM books;';
   client.query(instruction).then(function(sqlSaveData){
-    console.log(sqlSaveData.rows);
+    //console.log(sqlSaveData.rows);
     const booksArray = sqlSaveData.rows;
+    //console.log(booksArray);
     if(booksArray.length > 0){
       res.render('index', { booksArray });
     } else {
-      res.render('index');
+      res.redirect('/search');
     }
 
   });
 });
 
+
 //THIS WILL SAVE THE USER BOOKS TO SQL AND RENDER THEM TO SAVED BOOKS PAGE
 app.post('/user-books', (req, res) => {
-    let SQL = `INSERT INTO books
-    (author, title, isbn, image_url, summary, category)
-    VALUES($1,$2,$3,$4,$5,$6);`;
+  let SQL = `INSERT INTO books
+  (author, title, isbn, image_url, summary, category)
+  VALUES($1,$2,$3,$4,$5,$6);`;
 
-    let sqlData = [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.summary, req.body.category];
+  let sqlData = [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.summary, req.body.category];
 
-    // let SQLrow = (SQL, [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.summary, req.body.category]);
+  // let SQLrow = (SQL, [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.summary, req.body.category]);
 
-    client.query(SQL, sqlData)
-
-    const getAllBooks = 'SELECT * FROM books;';
-  client.query(getAllBooks).then(function(sqlSaveData){
-   // console.log(sqlSaveData.rows);
-    const booksArray = sqlSaveData.rows;
-    if(booksArray.length > 0){
-      res.render('savedBooks', { booksArray });
-    } else {
-      res.render('savedBooks');
-    }
-
+  client.query(SQL, sqlData).then(() => {
+    res.redirect('/');
   });
 
 
-  });
-
+});
 
   //THIS WILL DELETE A
 function deleteBook(req, res){
-    // console.log(req.query);
-     //console.log(req.params);
-    //console.log(req.body.id);
-    client.query('DELETE FROM books WHERE id=$1', [req.params.id]).then(result=>{
-    console.log(req.params.id);
+  console.log("HELLO", req.body.id);
+    client.query('DELETE FROM books WHERE id=$1', [req.body.id]).then(result=>{
+    //console.log(req.params.id);
     res.redirect('/');
     });
 }
 app.post('/searches', (req, res) => {
-  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.query}+in${req.body.search}`).then(data => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=+in${req.body.search}:${req.body.query}`).then(data => {
     const books = data.body.items.map(book => new Book(book));
     res.render('searchList', { books });
   }).catch(error => {
@@ -96,12 +85,12 @@ app.post('/searches', (req, res) => {
 });
 
 function Book(bookObj) {
-  this.image_url = bookObj.volumeInfo.imageLinks && bookObj.volumeInfo.imageLinks.thumbnail;
-  this.title = bookObj.volumeInfo.title;
-  this.author = bookObj.volumeInfo.authors;
-  this.summary = bookObj.volumeInfo.description;
-  this.categorie = bookObj.volumeInfo.categories;
-  this.isbn = bookObj.volumeInfo.industryIdentifiers[0].identifier;
+  this.image_url = bookObj.volumeInfo.imageLinks && bookObj.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpeg';
+  this.title = bookObj.volumeInfo.title || 'undefined';
+  this.author = bookObj.volumeInfo.authors || 'undefined';
+  this.summary = bookObj.volumeInfo.description || 'undefined';
+  this.categorie = bookObj.volumeInfo.categories || 'undefined';
+  this.isbn = bookObj && bookObj.volumeInfo && bookObj.volumeInfo.industryIdentifiers && bookObj.volumeInfo.industryIdentifiers[0] && bookObj.volumeInfo.industryIdentifiers[0].identifier || 'undefined';
 }
 
 app.listen(PORT, () => console.log(`app running on ${PORT}`));
